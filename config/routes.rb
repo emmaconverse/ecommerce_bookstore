ca777dd281b522adc72d5e5c39fe74a7237a9426
Emma Converse <emmaconverse@gmail.com>
2018-10-13 17:12:29 -0400

    initial controller setup and routes tidying

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+resources :adminbooks
+resources :books, only: [:index, :show]
+resources :authors, only: [:index, :show]
+resources :home, only: [:index]
+resources :sales
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root to: "home#index"
