716fdbbd249a088caf37f561dd0e26c1e08e7e0d
Emma Converse <emmaconverse@gmail.com>
2018-10-14 14:17:55 -0400

    changed devise root url to books, sign out now works

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ resources :home, only: [:index]
 resources :sales
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root to: "home#index"
+   root to: "books#index"
 end
