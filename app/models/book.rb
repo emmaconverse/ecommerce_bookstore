f9e9cf0a5fdbe157cd8b7e4f41dd8749dc471374
Emma Converse <emmaconverse@gmail.com>
2018-10-21 18:12:04 -0400

    refactored books controller to one, added actions to author, images broken on index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,8 @@
 class Book < ApplicationRecord
   has_one_attached :book_cover
-
   belongs_to :author, class_name: "User"
 
-  # accepts_nested_attributes_for :author, class_name: "User"
+  accepts_nested_attributes_for :author
 
   has_many :sales
   has_many :users, through: :sales
