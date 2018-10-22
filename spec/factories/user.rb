606a962a74d7ea7ab2b21301ab88d8285d62d6bb
Emma Converse <emmaconverse@gmail.com>
2018-10-22 12:28:05 -0400

    added rspec and faker, fixed image display issue

---
+++ spec/factories/user.rb
@@ -0,0 +1,19 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :user do
+    name { "Dalek Laserlights" }
+    email { Faker::Internet.email }
+    password { "dalekdalek" }
+
+    factory :author do
+      author { true }
+    end
+
+    # for admin, default is fault so do not need to set, if true need to set it
+    factory :admin do
+      admin { true }
+    end
+
+  end
+end
